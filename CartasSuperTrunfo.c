df36998c5447d4a8b21431dd82b405ec6fbaa148
vitor9dias <vitorh9d@gmail.com>
2025-04-09 22:54:06 +0000

    SupertrunfoINTERMEDIO..

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,71 +1,85 @@
+#include <stdio.h>
 
-// carta 1: Piauí
 int main (){
-    char nome1 [30];
-    char codigo1 [20];
-    int populacao1;
-    float area1;
-    int pib1;
-    int turistico1;
-   
-    printf("Carta1: \n")
-    printf("Nome do Estado: \n");
-    scanf("%s", nome1 );
+   char nome1 [30];
+   char codigo1 [20];
+   int populacao1;
+   float area1;
+   float pib1;
+   int turistico1;
+   float densidade1;
+   float percapta1;
   
-     printf("Codigo do Estado: \n")
-     scanf("%s", codigo1 );
-  
-     printf("População: \n");
-     scanf("%d", &populacao1 );
-  
-     printf("area: \n");
-     scanf("%f", &area1);
-  
-     printf("Produto Interno Bruto: \n");
-     scanf("%d", &pib1);
-      
-     printf("Quantidade de pontos turísticos: \n");
-     scanf("%d", &turistico1);
-  
-  
-    printf("Nome do Estado: %s - Codigo: %s", nome1, codigo1);
-    printf("Populacao: %d - PIB: %d - Quantidade de pontos turisticos: %d", populacao1 , pib1, turistico1);
-    prinf("Area: %f", area);
-  
-    //carta2: 
-    char nome2 [30];
-    char codigo2 [10];
-    int populacao2;
-    float area2;
-    int pib2;
-    int turistico2;
-  
-    printf("Nome do Estado: \n");
-    scanf("%s", nome2 );
-  
-     printf("Codigo do Estado: \n")
-     scanf("%s", codigo2);
-  
-     printf("População: \n");
-     scanf("%d", &populacao2 );
-  
-     printf("area: \n");
-     scanf("%f", &area2);
-  
-     printf("Produto Interno Bruto: \n");
-     scanf("%d", &pib2);
-      
-     printf("Quantidade de pontos turísticos: \n");
-     scanf("%d", turistico2);
-  
-  
-    printf("Nome do Estado: %s - Codigo: %s", nome2, codigo2);
-    printf("Populacao: %d - PIB: %d - Quantidade de pontos turisticos: %d", populacao2 , pib2 , turistico2);
-    prinf("Area: %f", area2);
-  
-  
-  
-  return 0;
-  
-  
-     }
+   printf("Carta1: \n");
+   printf("Nome do Estado: \n");
+   scanf("%s", nome1 );
+ 
+    printf("Codigo do Estado: \n");
+    scanf("%s", codigo1 );
+ 
+    printf("População: \n");
+    scanf("%d", &populacao1 );
+ 
+    printf("area: \n");
+    scanf("%f", &area1);
+ 
+    printf("Produto Interno Bruto: \n");
+    scanf("%f", &pib1);
+     
+    printf("Quantidade de pontos turísticos: \n");
+    scanf("%d", &turistico1);
+
+//media da densidade populacional 
+    densidade1= (pib1 + area1)/ 2;
+    percapta1= (float) (pib1 +populacao1) / 2; //pib por cabeça
+
+ 
+ 
+   printf("Nome do Estado: %s - Codigo: %s", nome1, codigo1);
+   printf("Populacao: %d   - PIB: %.2f   - Quantidade de pontos turisticos: %d", populacao1 , pib1, turistico1);
+   printf("Area: %.2f", area1);
+   printf("Densidade Populacional:%.2f\n", densidade1);
+   printf("Produto Interno Bruto por pessoa: %.2f\n", percapta1); 
+
+ 
+   //carta2: 
+   char nome2 [30];
+   char codigo2 [10];
+   int populacao2;
+   float area2;
+   float pib2;
+   int turistico2;
+   float densidade2;
+   float percapta2;
+
+   printf("Nome do Estado: \n");
+   scanf("%s", nome2 );
+ 
+    printf("Codigo do Estado: \n");
+    scanf("%s", codigo2);
+ 
+    printf("População: \n");
+    scanf("%d", &populacao2 );
+ 
+    printf("area: \n");
+    scanf("%f", &area2);
+ 
+    printf("Produto Interno Bruto: \n");
+    scanf("%f", &pib2);
+     
+    printf("Quantidade de pontos turísticos: \n");
+    scanf("%d", &turistico2);
+ 
+    densidade2= (pib2 + area2)/ 2;// densidade populacional
+   percapta2= (float) (pib2 +populacao2) / 2; // pib por cabeça
+ 
+   printf("Nome do Estado: %s - Codigo: %s", nome2, codigo2);
+   printf("Populacao: %d - PIB: %.2f - Quantidade de pontos turisticos: %d", populacao2 , pib2 , turistico2);
+   printf("Area: %f", area2);
+   printf("Densidade:%.2f \n",densidade2);
+   printf("Pib percapta: %.2f \n", percapta2); 
+ 
+ return 0;
+ 
+ 
+    }
